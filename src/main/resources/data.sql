1068aef8952ce13e6a9ea6526765004f9eac4d3f
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2022-03-19 14:55:29 +0300

    Spring Boot JPA. Types of relationships between tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
-insert into films(title) values ('Harry Potter');
-insert into films(title) values ('Back to the future');
+insert INTO GENRE(ID ,NAME) VALUES (1, 'fantasy');
+insert INTO GENRE(ID ,NAME) VALUES (2, 'fantastic');
+
+insert into films(ID, TITLE, GENRE_ID) values (1, 'Harry Potter', 1);
+insert into films(ID, TITLE, GENRE_ID) values (2, 'Back to the future', 2);
+
+insert INTO PLACE(ID ,NAME, FILM_ID) VALUES (1,'London', 1);
+insert INTO PLACE(ID ,NAME, FILM_ID) VALUES (2,'Train', 1);
+insert INTO PLACE(ID ,NAME, FILM_ID) VALUES (3,'USA',1);
